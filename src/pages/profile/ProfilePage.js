9ae7eebb149df622732629867939a067868616f1
raoul <rritmanto@gmail.com>
2023-11-14 20:33:20 +0700

    aman

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,33 +1,34 @@
 import React from "react";
 import "./index.css";
+import { Link, useParams } from "react-router-dom";
 const profileData = [
   {
     nama: "Rajendra Aurelius Ritmanto",
-    nim: "21120121130050",
-    fotoUrl:"https://avatars.githubusercontent.com/u/116475964?v=4"
+    nim: "21120121140155",
+    fotoUrl:"https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
       
   },
   {
-    nama: "zidane romandhon putra ",
-    nim: "21120121140115",
-    fotoUrl:"https://avatars.githubusercontent.com/u/116475964?v=4"  
+    nama: "Athaillah Daffa Dhiyaul-Haq ",
+    nim: "21120121140099",
+    fotoUrl:"https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"  
   },
   {
-    nama: "Rafly an nindra fitrah pratama",
-    nim: "21120121120002",
-    fotoUrl:"https://avatars.githubusercontent.com/u/116475964?v=4"  
+    nama: "Lina Silfiyah",
+    nim: "21120121120027",
+    fotoUrl:"https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"  
   },
   {
-    nama: "Siti Isyaroh",
-    nim: "21120121120007",
-    fotoUrl:"https://avatars.githubusercontent.com/u/116475964?v=4"  
+    nama: "Tanu Prahatama Hafiyyan",
+    nim: "21120121120083",
+    fotoUrl:"https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"  
   },
 ];
 
 function ProfilePage() {
   return (
     <div>
-      <h1>Profil</h1>
+      <h1>Team Members</h1>
       <div className="profile-cards">
         {profileData.map((data, index) => (
           <div className="profile-card" key={index}>
@@ -37,6 +38,11 @@ function ProfilePage() {
           </div>
         ))}
       </div>
+      <div className="profile-cards">
+        <button className="home-btn">
+        <Link to="/">Back to Home</Link>
+        </button>
+        </div>
     </div>
   );
 }
